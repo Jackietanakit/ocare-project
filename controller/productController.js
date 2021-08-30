2197531cc60f9bb3720395b770268badc663faee
Jackietanakit <Jackietanakit@gmail.com>
2021-08-30 21:23:29 +0700

    Update productController.js

--- controller/productController.js
+++ controller/productController.js
@@ -1,7 +1,7 @@
-const db = require("../database/db");
+const firebase = require("../database/db");
 const Product = require("../models/product");
 const { uploadProduct } = require("../helper/uploadImage");
-const firestore = db.firestore();
+const firestore = firebase.firestore();
 
 const addProduct = async (req, res) => {
   try {
@@ -19,7 +19,7 @@ const addProduct = async (req, res) => {
       data.mainImage = imagesUrl[0];
       data.images = imagesUrl.splice(1);
       data.uid = uid;
-      data.time = Date.now()
+      data.time = Date.now();
 
       // Upload text to firestore
       await firestore.collection("Product").doc().set(data);
@@ -45,7 +45,7 @@ const getProduct = async (req, res) => {
         data.data().images,
         data.data().description,
         data.data().price,
-        data.data().category,
+        data.data().category
       );
       res.send(product);
     }
@@ -56,41 +56,58 @@ const getProduct = async (req, res) => {
 
 // Still editing :(
 const editProduct = async (req, res) => {
-  try {
-    if (!req.body && !req.files) {
-      res.status(400).send("Nothing to change");
-    } else {
-      const data = req.body;
-      const uid = req.uid;
-      var productRef = firestore.collection("Product").doc(data.id);
-      if (req.files) {
-        const imagesUrl = await uploadProduct(req.files, uid);
-        data.mainImage = imagesUrl.splice(0, 1);
-        for (i in imagesUrl) {
-          if (imagesUrl[i]) {
-            await productRef.update({
-              images: firebase.firestore.i.arrayRemove(i),
-            });
-          }
-        }
-      }
+  if (!req.body && !req.files) {
+    res.status(400).send("Nothing to change");
+  } else {
+    const data = req.body;
+    const images = req.files;
+    const uid = req.uid;
 
-      for (const property in req.body) {
-        if (!data[property]) {
-          delete data[property];
-        }
-      }
+    console.log(data);
 
-      // delete id before edit
-      delete data.id;
-      await firestore
-        .collection("Profile")
-        .doc(req.body.id)
-        .set(data, { merge: true });
-      res.status(200).send("Ok");
+    var productRef = firestore.collection("Product").doc(data.id);
+
+    // delete images in Firebase
+    for (i in data.imagesDelete) {
+      await productRef.update({
+        images: firebase.firestore.FieldValue.arrayDelete(data.imagesDelete[i]),
+      });
     }
-  } catch {
-    res.status(400).send(error.message);
+
+    if (data.mainImgExist == "true") {
+      const mainImageUrl = await uploadProduct(
+        images.splice(0, 1),
+        uid,
+        "edit"
+      );
+      data.mainImage = mainImageUrl;
+    }
+    const uploadImages = images;
+
+    // upload images to Firebase
+    if (uploadImages) {
+      const imagesUrl = await uploadProduct(uploadImages, uid, "edit");
+      console.log(imagesUrl);
+      for (i in imagesUrl) {
+        await productRef.update({
+          images: firebase.firestore.FieldValue.arrayUnion("Yay"),
+        });
+      }
+    }
+
+    for (const property in data) {
+      if (!data[property]) {
+        delete data[property];
+      }
+    }
+
+    // delete id before edit
+    delete data.mainImgExist;
+    delete data.id;
+    console.log(data);
+    // await productRef
+    //   .set(data, { merge: true });
+    res.status(200).send("Ok");
   }
 };
 
