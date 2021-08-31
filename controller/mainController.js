f2dad9bea3eb7fdcd080120bedafe01b97d0db8f
ployloyloy <ployloyloy06@gmail.com>
2021-09-01 03:30:34 +0700

    update main

--- controller/mainController.js
+++ controller/mainController.js
@@ -6,6 +6,8 @@ const getMainComponent = async (req, res) => {
   try {
     // store last document
     let lastestDoc = null;
+    let category = "";
+    let index = null;
 
     if (!req.body.lastestDocTime) {
       lastestDoc = null;
@@ -13,60 +15,16 @@ const getMainComponent = async (req, res) => {
       lastestDoc = parseInt(req.body.lastestDocTime);
     }
 
-    // Create Product Ref
-    const productRef = await firestore
-      .collection("Product")
-      .orderBy("time")
-      .limit(10)
-      .startAfter(lastestDoc || 0);
-    const productData = await productRef.get();
-
-    const productsInfoArray = [];
-    const uidArray = [];
-
-    if (productData.empty) {
-      res.status(404).send("No product record");
+    if (!req.body.category) {
+      category = "";
     } else {
-      productData.forEach((doc) => {
-        const mainComponent = new Maincomponent(
-          doc.id,
-          doc.data().productName,
-          doc.data().mainImage,
-          doc.data().images,
-          doc.data().description,
-          doc.data().time
-        );
-        productsInfoArray.push(mainComponent);
-        uidArray.push(doc.data().uid);
-        //console.log(productData);
-      });
-
-      // add profile data (username, pic) to maincomponent
-      for (i = 0; i < productsInfoArray.length; i++) {
-        const profileInfo = await firestore
-          .collection("Profile")
-          .doc(uidArray[i]);
-        let profileData = await profileInfo.get();
-        productsInfoArray[i].username = profileData.data().displayName;
-        productsInfoArray[i].userImage = profileData.data().userImage;
-      }
-      res.send(productsInfoArray);
+      category = String(req.body.category);
     }
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
-};
 
-const searchMainComponent = async (req, res) => {
-  try {
-    // store last document
-    let lastestDoc = null;
-    const category = String(req.body.category);
-
-    if (!req.body.lastestDocTime) {
-      lastestDoc = null;
+    if (!req.body.index) {
+      index = null;
     } else {
-      lastestDoc = parseInt(req.body.lastestDocTime);
+      index = parseInt(req.body.index);
     }
 
     // Create Product Ref
@@ -95,44 +53,48 @@ const searchMainComponent = async (req, res) => {
         productsInfoArray.push(mainComponent);
         uidArray.push(doc.data().uid);
       });
-      //console.log(uidArray);
 
       let search = [];
-      let y = 0;
-      for (i in productsInfoArray) {
-        if (
-          productsInfoArray[i].category.toLowerCase() == category.toLowerCase()
-        ) {
-          search.push(productsInfoArray[i]);
-        } else {
-        }
-      }
-
-      let counter = 0;
-      // add profile data (username, pic) to maincomponent
-      for (i in search) {
-        const profileInfo = await firestore
-          .collection("Profile")
-          .doc(uidArray[i]);
-        let profileData = await profileInfo.get();
-        search[i].username = profileData.data().displayName;
-        search[i].userImage = profileData.data().userImage;
-        counter++;
-      }
-      // console.log(counter);
-
-      let index = parseInt(req.body.index);
-      let tripArray = [];
-      if (index > 0) {
-        if (counter - index * 10 > 0) {
-          tripArray = search.splice(index * 10);
-          res.send(tripArray);
-        } else {
-          res.status(404).send("no more products!");
+      if (category != "") {
+        for (i in productsInfoArray) {
+          if (
+            productsInfoArray[i].category.toLowerCase() ==
+            category.toLowerCase()
+          ) {
+            search.push(productsInfoArray[i]);
+          }
         }
       } else {
-        tripArray = search.splice(0, 10);
-        res.send(tripArray);
+        search.push(...productsInfoArray);
+      }
+
+      // add profile data (username, pic) to maincomponent
+      if (!search.empty) {
+        let counter = 0;
+        for (i in search) {
+          const profileInfo = await firestore
+            .collection("Profile")
+            .doc(uidArray[i]);
+          let profileData = await profileInfo.get();
+          search[i].username = profileData.data().displayName;
+          search[i].userImage = profileData.data().userImage;
+          counter++;
+        }
+        const result = tripArray(index, counter, search);
+        res.send(result);
+      } else {
+        let counter = 0;
+        for (i = 0; i < productsInfoArray.length; i++) {
+          const profileInfo = await firestore
+            .collection("Profile")
+            .doc(uidArray[i]);
+          let profileData = await profileInfo.get();
+          productsInfoArray[i].username = profileData.data().displayName;
+          productsInfoArray[i].userImage = profileData.data().userImage;
+          counter++;
+        }
+        const result = tripArray(index, counter, productsInfoArray);
+        res.send(result);
       }
     }
   } catch (error) {
@@ -140,7 +102,22 @@ const searchMainComponent = async (req, res) => {
   }
 };
 
+function tripArray(num, counter, array) {
+  let index = parseInt(num);
+  let tripArray = [];
+  if (index > 0) {
+    if (counter - index * 10 > 0) {
+      tripArray = array.splice(index * 10);
+      return tripArray;
+    } else {
+      return "No more products!";
+    }
+  } else {
+    tripArray = array.splice(0, 10);
+    return tripArray;
+  }
+}
+
 module.exports = {
   getMainComponent,
-  searchMainComponent,
 };
--- routes/main-route.js
+++ routes/main-route.js
@@ -1,13 +1,9 @@
 const express = require("express");
-const {
-  getMainComponent,
-  searchMainComponent,
-} = require("../controller/mainController");
+const { getMainComponent } = require("../controller/mainController");
 
 const router = express.Router();
 
 router.get("/", getMainComponent);
-router.get("/search", searchMainComponent);
 
 module.exports = {
   routes: router,
