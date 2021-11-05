14e2b0f09da1782c6ebfc32c37b94b57784a0932
Jackie <jackietanakit@gmail.com>
2021-11-06 02:56:14 +0700

    f

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const profileRoute = require("./routes/profile-route");
 const mainRoute = require("./routes/main-route");
 const authenRoute = require("./routes/authen-route");
 const cors = require("cors");
+const { credential } = require("firebase-admin");
 
 require("dotenv").config();
 
@@ -12,7 +13,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://ocare-marketplace-ffa5a.web.app/",
+    credential: true,
+  })
+);
 
 app.use("/product", productRoutes.routes);
 app.use("/profile", profileRoute.routes);
