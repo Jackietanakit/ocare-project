7ee88003301a746f396d81c164393797813f5b1b
Jackie <jackietanakit@gmail.com>
2021-11-06 03:02:47 +0700

    cors

--- server.js
+++ server.js
@@ -9,16 +9,10 @@ const { credential } = require("firebase-admin");
 require("dotenv").config();
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credential: true,
-  })
-);
 
 app.use("/product", productRoutes.routes);
 app.use("/profile", profileRoute.routes);
