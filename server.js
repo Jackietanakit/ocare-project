50f154e539cb140de813827659d4454c50fd11b7
Jackie <jackietanakit@gmail.com>
2021-11-06 02:59:42 +0700

    dd

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://ocare-marketplace-ffa5a.web.app/",
+    origin: "http://localhost:3000",
     credential: true,
   })
 );
